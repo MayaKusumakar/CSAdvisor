98a384631fb5c6421de0deec7f09ad941a32c850
Rico Rodriguez <ricopassanisi@gmail.com>
2025-04-20 00:34:27 -0700

    Example API call snippet

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"SwipeNGo/pkg/mongoApi"
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
 )
 
 func main() {
 
-	mongoClient, _ := mongoApi.GetMongoClient()
-	collection, err := mongoApi.GetCollection(mongoClient, "Some", "Thing")
-	fmt.Println(collection, err)
+	// 	mongoClient, _ := mongoApi.GetMongoClient()
+	// 	collection, err := mongoApi.GetCollection(mongoClient, "Some", "Thing")
+	// 	fmt.Println(collection, err)
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	//router := gin.Default()
-	//router.GET("/ping", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"message": "pong",
-	//	})
-	//})
-	//router.Run() // listen and serve on 0.0.0.0:8080
+	router.POST("/ping", func(c *gin.Context) {
+		jsonData, _ := io.ReadAll(c.Request.Body)
+		c.JSON(200, gin.H{
+			"message":      "pong",
+			"message_body": string(jsonData),
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
--- frontend/SwipeNGo/app/(tabs)/index.tsx
+++ frontend/SwipeNGo/app/(tabs)/index.tsx
@@ -7,9 +7,55 @@ import { ThemedView } from '@/components/ThemedView';
 import { Button, ButtonText } from "@/components/ui/button"
 import LoginButton from "@/components/login/LoginButton";
 import {useNavigation} from "@react-navigation/native";
+import {useEffect, useState} from "react";
+import { Text } from '@/components/ui/text';
 
 
 export default function HomeScreen() {
+
+   let [someState, setSomeState] = useState("Loading...");
+
+   // To make an API call, you need to have it in a useEffect react hook.
+    // You can either trigger the api call on page load with no dependencies,
+    // or whenever a state changes by adding the state to the list of dependencies
+  useEffect(() => {
+      // In the useEffect, you must declare an async function as follows to make the api call
+      // Call fetch in your async function, and after your function declareation, call the function
+      // Most of your logic should be in the async function
+      // Consider having a state variable that tracks the function progress,
+      // ie; a loading state variable that is true while the async function is working, and false when the function finishes
+    const apiCall = async () => {
+      const postBody = {
+        description: "You want to send data to the backend with JSON",
+        more_description: "You can convert javascript objects to json later with JSON.stringify(object)",
+        even_more_description: "Most fetches will be POST requests also because we want to send JSON data to the backend",
+        even_more_more_description: "Standard convention for this is POST requests for REST API",
+        even_more_more_more_description: "Use the below snippet to make requests to the backend"
+      }
+      const response: Response = await fetch(
+          'http://localhost:8080/ping', {
+            method: 'POST',
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postBody)
+          }
+      )
+      if (response.status === 200) {
+        // If status OK
+        // the data returned from the API will be in JSON
+          // You can extract the json from the response using this line of code
+        const data = await response.json();
+        // data is a json object with your data.
+
+          setSomeState(JSON.stringify(data))
+      } else {
+        setSomeState("Error: Did not receive response status 200: " + response.status);
+      }
+    }
+    apiCall();
+  }, [])
+
   const navigation = useNavigation();
   return (
     <ParallaxScrollView
@@ -20,10 +66,10 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-
         <Button size="md" variant="solid" action="primary" onPress={() => navigation.navigate('login')}>
             <ButtonText>Hello World!</ButtonText>
         </Button>
+      <Text>{someState}</Text>
     </ParallaxScrollView>
   );
 }
